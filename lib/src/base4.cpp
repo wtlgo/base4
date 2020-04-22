f7697e0890a3c97fce6310dbafb9eabc238f99ce
wtlgo <georgeendo0@gmail.com>
2020-04-22 18:50:39 +0300

    Keeping things const

--- lib/src/base4.cpp
+++ lib/src/base4.cpp
@@ -34,10 +34,13 @@ std::string wtlgo::base4_decode(const std::string& input, const std::array<char,
         throw std::runtime_error("Invalid input: Found characters that are not in the current char set.");
     }
 
-    std::unordered_map<char, uint8_t> char_map;
-    for(uint8_t i = 0; i < charset.size(); ++i) {
-        char_map[charset[i]] = i;
-    }
+    const std::unordered_map<char, uint8_t> char_map = [&charset]{
+        std::unordered_map<char, uint8_t> res;
+        for(uint8_t i = 0; i < charset.size(); ++i) {
+            res[charset[i]] = i;
+        }
+        return res;
+    }();
     
     std::string res;
     for(auto it = input.cbegin(); it != input.cend(); it += 4) {
